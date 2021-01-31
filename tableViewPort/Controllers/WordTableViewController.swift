a20d2f6e3ab303c0407dc3dbe54da770c4d61562
Kumar Shubham <kumar.shubham@redbus.com>
2021-01-31 18:34:47 +0530

    Initial Commit

---
+++ tableViewPort/Controllers/WordTableViewController.swift
@@ -0,0 +1,16 @@
+//
+//  WordTableViewController.swift
+//  tableViewPort
+//
+//  Created by Kumar Shubham on 31/01/21.
+//
+
+import Foundation
+import UIKit
+
+class WordTableViewController : UIViewController, UITableViewDelegate{
+    
+    override func viewDidLoad() {
+        
+    }
+}
---
+++ tableViewPort/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tableViewPort
+//
+//  Created by Kumar Shubham on 31/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
