6b9d6d92f03c2c6a7d0d0a033f2e660618de30de
Kumar Shubham <kumar.shubham@redbus.com>
2021-01-31 19:17:54 +0530

    Empty TableView created programatically

--- tableViewPort/Controllers/WordTableViewController.swift
+++ tableViewPort/Controllers/WordTableViewController.swift
@@ -10,7 +10,24 @@ import UIKit
 
 class WordTableViewController : UIViewController, UITableViewDelegate{
     
+    let tableView = UITableView()
+    
     override func viewDidLoad() {
-        
+        setupTableView()
     }
+    
+    func setupTableView(){
+        view.addSubview(tableView)
+        
+        tableView.translatesAutoresizingMaskIntoConstraints                     = false
+        setupTableViewConstraints()
+    }
+    
+    func setupTableViewConstraints(){
+        tableView.topAnchor.constraint(equalTo: view.topAnchor).isActive        = true
+        tableView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive      = true
+        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive  = true
+        tableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive    = true
+    }
+    
 }
