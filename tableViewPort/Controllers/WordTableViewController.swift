6374002278adf1be47c519879b7a3577a600583d
Kumar Shubham <kumar.shubham@redbus.com>
2021-01-31 19:32:11 +0530

    changed bottom and top constraints to refer safeArea || changed the backgroundColor of the view

--- tableViewPort/Controllers/WordTableViewController.swift
+++ tableViewPort/Controllers/WordTableViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class WordTableViewController : UIViewController, UITableViewDelegate{
     
-    let tableView = UITableView()
+    let tableView   = UITableView()
+    var safeArea    : UILayoutGuide!
     
     override func viewDidLoad() {
+        
+        view.backgroundColor = UIColor(red: 0.75, green: 0.01, blue: 0.18, alpha: 1.00) //lipstick red
         setupTableView()
     }
     
@@ -20,14 +23,15 @@ class WordTableViewController : UIViewController, UITableViewDelegate{
         view.addSubview(tableView)
         
         tableView.translatesAutoresizingMaskIntoConstraints                     = false
+        safeArea = view.layoutMarginsGuide
         setupTableViewConstraints()
     }
     
     func setupTableViewConstraints(){
-        tableView.topAnchor.constraint(equalTo: view.topAnchor).isActive        = true
-        tableView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive      = true
-        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive  = true
-        tableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive    = true
+        tableView.topAnchor.constraint(equalTo: safeArea.topAnchor).isActive        = true
+        tableView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive          = true
+        tableView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor).isActive  = true
+        tableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive        = true
     }
     
 }
